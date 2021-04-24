8004ab5ed3ab52f7162feac7ad783b3fce25454c
Felipe <fcaetano630@gmail.com>
2021-04-24 15:14:26 -0300

    Adicionando arquivo

---
+++ src/main/java/application/Program.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+
+
+import chess.ChessMatch;
+
+
+/**
+ *
+ * @author Felipe
+ */
+public class Program {
+    public static void main(String[] args) {
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+    }
+    
+}
